7768ba9463724120bb1c9dbb16e896eb908cc60b
Lakjeewa Wijebandara <lakjeewaW@users.noreply.github.com>
2022-04-12 17:01:13 +0530

    create Task and Subtask interfaces in commons package

---
+++ todo-commons/src/interfaces.ts
@@ -0,0 +1,16 @@
+
+export interface Task {
+    _id?: string;
+    created_at?: Date;
+    title: string;
+    status: string;
+    subtasks: SubTask[];
+}
+
+export interface SubTask {
+    _id?: string;
+    todo_id: string;
+    created_at?: Date;
+    title: string;
+    status: string;
+}
